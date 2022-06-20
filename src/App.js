56a9952bcd25980c74b302ce6b1986e3a003a890
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2022-06-20 10:42:01 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { useState } from 'react';
 
 function sayHello() {
-  console.log(App.message)
+  console.log(App().message)
 }
 function printApp() {
   console.log("a")
@@ -25,7 +25,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
           onChange={handleChange}
         </p>
 
@@ -41,15 +40,7 @@ function App() {
 
         <button onClick={sayHello.bind(this)}>  Activate Lasers
         </button>
-               
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
