a0b9ee28a216d60f1ab59a71f751b12a2ed63989
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2022-06-20 11:14:49 +0200

    input functional (green)

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import './App.css';
 import { useState } from 'react';
 
 function sayHello() {
+  console.log(arr)
 }
+
+const arr = ['']
+const valArr = ['']
 
 function App() {
 
@@ -15,19 +19,24 @@ function App() {
     setMessage(event.target.value);
     console.log('value is:', event.target.value);
     arr[a] = event.target.value
+    valArr[a] = event.target.value
     printMany()  
   }
-  var arr = ['']
 
-  const handleChange = event => {
-    handle(4, event)
+  const handleChangeA = event => {
+    handle(0, event)
   };
-
   const handleChangeB = event => {
-    setMessageB(event.target.value);
-    console.log('value is:', event.target.value);
-    arr[3] = event.target.value
-    printMany()
+    handle(1, event)
+  };
+  const handleChangeC = event => {
+    handle(2, event)
+  };
+  const handleChangeD = event => {
+    handle(3, event)
+  };
+  const handleChangeE = event => {
+    handle(4, event)
   };
 
 
@@ -35,25 +44,23 @@ function App() {
     console.log("arr: " + arr)
   }
 
-
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          onChange={handleChange}
+          onChange={arr}
         </p>
 
         <div className='Green'>
-          <input type="text" maxLength="1" className="App-Green-0" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1"/>
-          <input type="text" maxLength="1" className="App-Green-1" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1"/>
-          <input type="text" maxLength="1" className="App-Green-2" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1"/>
-          <input type="text" maxLength="1" className="App-Green-3" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1"onChange={handleChange} value={message}/>
-          <input type="text" maxLength="1" className="App-Green-4" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={handleChangeB} value={messageB}/>
+          <input type="text" maxLength="1" className="App-Green-0" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={handleChangeA} value={valArr[0]}/>
+          <input type="text" maxLength="1" className="App-Green-1" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={handleChangeB} value={valArr[1]}/>
+          <input type="text" maxLength="1" className="App-Green-2" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={handleChangeC} value={valArr[2]}/>
+          <input type="text" maxLength="1" className="App-Green-3" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={handleChangeD} value={valArr[3]}/>
+          <input type="text" maxLength="1" className="App-Green-4" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={handleChangeE} value={valArr[4]}/>
         </div>
 
-        <h2>Message: {message}</h2>
+        <h2>Message: {String(arr)}</h2>
 
         <button onClick={sayHello.bind(this)}>  Activate Lasers
         </button>
