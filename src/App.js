ae5543c2ad310386548a7c9f2df76e3a946a446b
Hugh Coleman <46972988+hjc2@users.noreply.github.com>
2022-06-21 11:29:20 +0200

    dynamic handling
    Can't say I know why I didn't do this earlier

--- src/App.js
+++ src/App.js
@@ -99,6 +99,11 @@ function App() {
   const handleChangeJ = event => {
     handle(9, event)
   };
+
+  const handleChange = (event, num) => {
+    handle(num, event)
+  }
+  
   const printMany = event => {
     //console.log("arr: " + arr)
     //console.log(message)
@@ -113,8 +118,8 @@ function App() {
         </p>
 
         <div className='Green'>
-          <input type="text" maxLength="1" className="App-Green-0" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={handleChangeA} value={valArr[0]}/>
-          <input type="text" maxLength="1" className="App-Green-1" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={handleChangeB} value={valArr[1]}/>
+          <input type="text" maxLength="1" className="App-Green-0" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={(e) => {handleChange(e, 0)}} value={valArr[0]}/>
+          <input type="text" maxLength="1" className="App-Green-1" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={(e) => {handleChange(e, 1)}} value={valArr[1]}/>
           <input type="text" maxLength="1" className="App-Green-2" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={handleChangeC} value={valArr[2]}/>
           <input type="text" maxLength="1" className="App-Green-3" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={handleChangeD} value={valArr[3]}/>
           <input type="text" maxLength="1" className="App-Green-4" autoComplete="off" spellCheck="false" autoCorrect="off" data-index="1" onChange={handleChangeE} value={valArr[4]}/>
